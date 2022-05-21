9f645ab4760c7349f2e1eedf712470cf32745a75
jonhks <jonhgx@gmail.com>
2022-05-21 11:35:44 -0500

    Update/Actualizando repo con practica

---
+++ practica/app.js
@@ -0,0 +1,50 @@
+
+// // if(dia === 'Lunes'){
+//   //   'Me toca trabajar en la oficina'
+//   // } else if(dia === 'Martes'){
+//     //   'Me toca trabajaren casa'
+//     // } else if(dia === 'Miercoles'){
+//       //   'Me toca jugar Futbol'
+//       // } 
+      
+// let dia = prompt('Que día es hoy?'); // lunes
+// // lunes = lunes.charAt(0) = l
+//   //  l.toUpperCase() = L 
+//   //  L + dia.slice(1) unes = Lunes;
+// dia = dia.charAt(0).toUpperCase() + dia.slice(1);
+// console.log(dia)
+// let accionDelDia = '';
+
+// switch(dia){
+//   case 'Lunes':
+//     accionDelDia = 'me toca ir a la oficina'
+//   break; 
+//   case 'Martes':
+//     accionDelDia = 'Me toca trabajar en casa'
+//   break;
+//   case 'Miercoles':
+//     accionDelDia = 'Me toca jugar Fut'
+//   break;
+//   case 'Jueves':
+//     accionDelDia = 'Me toca salido con amigos'
+//   break;
+//   case 'Viernes':
+//     accionDelDia = 'Me toca ver Netflix'
+//   break;
+//   case 'Sabado':
+//     accionDelDia = 'Jugar en San Juan'
+//   break;
+//   case 'Domingo':
+//     accionDelDia = 'Me toca ir con el MC'
+//   break;
+//   default:
+//     accionDelDia = 'Ingresa un día entre el lunes y el domingo'
+//   break;  
+// }
+
+// console.log(accionDelDia);
+
+// || or
+// && and
+
+console.log(2 + 2 == 4 || 3 + 9 == 6)  
